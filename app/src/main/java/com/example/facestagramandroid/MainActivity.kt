f5e15b4d910a391b431f2afbf29e64dd4cc4474e
hutturo <ghdsuk95@naver.com>
2019-11-26 12:25:50 +0900

    start android

--- app/src/main/java/com/example/facestagramandroid/MainActivity.kt
+++ app/src/main/java/com/example/facestagramandroid/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.facestagramandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_view)
     }
+
+    fun buttonClick(view: View) {
+        val connection = URL("http://10.10.16.70/palce/1").openConnection()
+        val data = connection.inputStream.bufferedReader().readText()
+    }
+
 }
