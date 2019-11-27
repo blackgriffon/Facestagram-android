a0291eb2341f1c328c1f40b7b23088096de10a2a
hakumaku <gentlebuuny@gmail.com>
2019-11-27 09:04:09 +0900

    0904

--- app/src/main/java/com/example/facestagramandroid/MainActivity.kt
+++ app/src/main/java/com/example/facestagramandroid/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.facestagramandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import okhttp3.*
+import java.io.IOException
+
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_view)
+
     }
 
     fun buttonClick(view: View) {
-        val connection = URL("http://10.10.16.70/palce/1").openConnection()
-        val data = connection.inputStream.bufferedReader().readText()
+      //  val connection = URL("http://10.10.16.70/place/1").openConnection()
+      //  val data = connection.inputStream.bufferedReader().readText()
+
+        val url = "http://10.10.16.70:8085/place/1"
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException){
+                //에러 메세지 출력
+            }
+            override fun onResponse(call: Call, response: Response) {
+                println(response?.body()?.string())
+            }
+        })
+
     }
 
 }
